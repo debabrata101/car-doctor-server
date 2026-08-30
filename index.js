ebce6e6829c31fa18b02dc4648710e4d689fb12c
Debabrata Das <debabratacmt@gmail.com>
2026-08-30 23:28:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/jwt", (req, res) => {
 });
 
 // MongoDB
-const uri = `mongodb+srv://${process.env.local.DB_USER}:${process.env.local.DB_PASS}@cluster0.uckzgsu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uckzgsu.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
